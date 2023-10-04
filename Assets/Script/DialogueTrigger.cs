20de7b7ab4cce1d48d097fed8622dcb5c25c3a6d
Hikari <aufarafiqih2@gmail.com>
2023-10-04 23:37:01 +0700

    Menambah Dialogue
    Dialogue

---
+++ Assets/Script/DialogueTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogueScript;
+    private bool playerDetected;
+
+    //detect trigger with player
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") 
+        {
+            playerDetected = true;
+            dialogueScript.ToggleIndicator(playerDetected);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerDetected = false;
+            dialogueScript.ToggleIndicator(playerDetected);
+            dialogueScript.EndDialogue();
+        }
+    }
+
+    private void Update()
+    {
+        if (playerDetected && Input.GetKeyDown(KeyCode.E)) 
+        {
+            dialogueScript.StartDialogue();
+        }
+    }
+
+}
