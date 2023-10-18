372d692fec7733b3e1f54ba155258b63dbb60001
MatthewAM1 <105529464+MatthewAM1@users.noreply.github.com>
2023-10-19 00:45:02 +0800

    Enemy and shoot
    1. Enemy ant patrol
    2. Shoot now get destroyed after hitting enemy/block.

---
+++ Assets/Script/EnemyPatrol.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public GameObject pointLeft;
+    public GameObject pointRight;
+    private Rigidbody2D enemyRigidbody;
+    private Transform currentPoint;
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+        currentPoint = pointRight.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointRight.transform)
+        {
+            enemyRigidbody.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            enemyRigidbody.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointRight.transform)
+        {
+            FlipEnemy();
+            currentPoint = pointLeft.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointLeft.transform)
+        {
+            FlipEnemy();
+            currentPoint = pointRight.transform;
+        }
+    }
+
+    private void FlipEnemy()
+    {
+        Vector2 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointLeft.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointRight.transform.position, 0.5f);
+        Gizmos.DrawLine(pointLeft.transform.position, pointRight.transform.position);
+    }
+}
