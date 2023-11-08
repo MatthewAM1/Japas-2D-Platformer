7e7851b8aa789ff4e2fada05c62dd150212162d3
MatthewAM1 <105529464+MatthewAM1@users.noreply.github.com>
2023-11-08 11:17:13 +0800

    Npc Cage, Key, And Sewer
    1. Free Locked Npc using interaction
    2. Simple key guard (placeholder)
    3. Key to open sewer door
    4. Sewer door that can only be opened using key

--- Assets/Script/Enemy/EnemyHead.cs
+++ Assets/Script/Enemy/EnemyHead.cs
@@ -6,24 +6,6 @@ public class EnemyHead : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
 
-
-    private void Awake()
-    {
-        
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
