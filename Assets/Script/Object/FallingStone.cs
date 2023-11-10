7eb60eca45649356ecf7feaa19e5e17f24aba6cb
MatthewAM1 <105529464+MatthewAM1@users.noreply.github.com>
2023-11-10 17:12:42 +0800

    Falling Stone And Trampoline
    1. Add falling stone that triggered when player is near them.
    2. Add trampoline that player can use to get to higher place.

---
+++ Assets/Script/Object/FallingStone.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingStone : MonoBehaviour
+{
+    private Rigidbody2D fallingStoneRB;
+    [SerializeField] private float fallingSpeed;
+
+    private void Awake()
+    {
+        fallingStoneRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            fallingStoneRB.gravityScale = fallingSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            Debug.Log("Player kena batu");
+        }
+    }
+}
