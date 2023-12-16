2ad6a187033fbb106cc8560a42cc27b2ef447353
MatthewAM1 <105529464+MatthewAM1@users.noreply.github.com>
2023-12-16 16:03:00 +0800

    Update UI Baru
    1. Update UI Baru
    2. Membuat Scene untuk Cutscene game
    3. Mencoba fix enemy lalat penembak (gagal).

--- Assets/Script/Enemy/EnemyProjectile.cs
+++ Assets/Script/Enemy/EnemyProjectile.cs
@@ -4,11 +4,43 @@ public class EnemyProjectile : EnemyDamage
 {
     public float speed;
     Rigidbody2D rb;
+    private float projectileTimer = 2.0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         // Mengatur kecepatan peluru ke arah kiri
         rb.velocity = -transform.right * speed;
+    }
+
+    private void Update()
+    {
+        //DestroyProjectileTimer();
+    }
+
+    void DestroyProjectileTimer() // Hasil error karena peluru prefab di destroy duluan.
+    {
+        projectileTimer -= 1 * Time.deltaTime;
+
+        if (projectileTimer < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider) // Objek tetap tidak mau ke destroy meskipun kena player
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        if (collider.gameObject.CompareTag("Confiner"))
+        {
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
